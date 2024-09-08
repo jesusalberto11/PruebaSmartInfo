e41fdb072acdb86cb468f3e582b4432418834d08
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-09-08 14:02:06 -0500

    Feat: Added password comprobation with regex and comprobation with repeat password

--- Punto 1/classes/Register.class.php
+++ Punto 1/classes/Register.class.php
@@ -1,17 +1,31 @@
 <?php
 require_once("./controller/controller.php");
 require_once("./config/connection.php");
+require_once("./classes/Login.class.php");
 
 class Register
 {
     private $email;
     private $password;
+    private $repeatPassword;
     private $status;
 
-    public function __construct($email, $password)
+    public function __construct($email, $password, $repeatPassword)
     {
         $this->email = clear_user_input($email);
         $this->password = clear_user_input($password);
+        $this->repeatPassword = clear_user_input($repeatPassword);
+
+        if (!preg_match('/^(?=.*\*)(.{10,})$/', $this->password)) {
+            $this->status = ["status" => "error", "error" => "La contraseña debe tener al menos 10 caracteres y contener un asterisco (*)"];
+            return;
+        }
+
+        if ($this->password != $this->repeatPassword) {
+            $this->status = ["status" => "error", "error" => "Las contraseñas no coinciden."];
+            return;
+        }
+
         $this->password = hash_pass($this->password);
 
         try {
@@ -25,7 +39,12 @@ class Register
             $stmt->execute([$this->email, $this->password]);
 
             $this->status = ["status" => "success", "error" => null];
-            header("Location: index.php");
+
+            ob_start();
+            session_start();
+            $_SESSION['email'] = $this->email;
+            $_SESSION['valid'] = true;
+            header('Location: home.php');
         } catch (PDOException $e) {
             die("Can't create user!: " . $e->getMessage());
         }
