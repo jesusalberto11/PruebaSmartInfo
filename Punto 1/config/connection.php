46d770bd1257e21652dee75e60951f72216ac4bc
Jesus Alberto <jesusalbertomoralesrico@gmail.com>
2024-09-08 01:35:18 -0500

    Update: Directories path

--- Punto 1/config/connection.php
+++ Punto 1/config/connection.php
@@ -15,11 +15,10 @@ class Connection
             $connection = new Connection();
             $connection->connect = new PDO("{$connection->driver}:host={$connection->host};port={$connection->port};dbname={$connection->dbname}", $connection->user, $connection->password);
             $connection->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //echo "Conectado correctamente";
+
             return $connection->connect;
         } catch (PDOException $e) {
             die("Error: " . $e->getMessage());
         }
     }
 }
-//Connection::getConnection();
--- Punto 1/controller/controller.php
+++ Punto 1/controller/controller.php
@@ -1,5 +1,5 @@
 <?php
-require_once "C:/Apache24/htdocs/config/connection.php";
+require_once(__DIR__ . "/../config/connection.php");
 
 function clear_user_input($input)
 {
